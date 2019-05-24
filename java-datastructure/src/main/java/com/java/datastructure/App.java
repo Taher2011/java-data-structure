c19412e7d80f82292b4c00d725d6f7aa3f6f0dc7
Taher <tali20@dxc.com>
2019-05-24 09:47:29 +0530

    FirstCommit

---
+++ java-datastructure/src/main/java/com/java/datastructure/App.java
@@ -0,0 +1,13 @@
+package com.java.datastructure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
