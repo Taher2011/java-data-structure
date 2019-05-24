b3c03d1cca273340bb2d4c37682fb923a3b160ee
Taher <tali20@dxc.com>
2019-05-24 09:57:03 +0530

    ReverseString

--- java-datastructure/src/main/java/reversestring/ReverseString.java
+++ java-datastructure/src/main/java/reversestring/ReverseString.java
@@ -2,8 +2,21 @@ package reversestring;
 
 public class ReverseString {
 
-	public static void main(String[] args) {
+	public static String reverseString(String str) {
 
+		String reverseStr = "";
+
+		for (int i = str.length() - 1; i >= 0; i--) {
+			char c = str.charAt(i);
+			reverseStr += c;
+		}
+		return reverseStr;
+	}
+
+	public static void main(String[] args) {
+		String str = "Taher";
+		String reverseStr = reverseString(str);
+		System.out.println("Reverse of " + str + " is " + reverseStr);
 	}
 
 }
